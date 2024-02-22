8c3e2a99302698ccf83e99bc5c3f5bd585e019f8
PN <95763550+PN-Tester@users.noreply.github.com>
2024-02-22 11:21:05 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -11,3 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementsByClassName("extendo-title")[0].addEventListener("click", function() {
+    var newURL = "https://github.com/PN-Tester/Extendo";
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.update(activeTab.id, { url: newURL });
+    });
+  });
+});
